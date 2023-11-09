b6af263f10b5baca696a4a9c836796077a4ea814
connect2avni <smrutictc100@gmail.com>
2023-11-09 16:19:09 +0530

    changes

---
+++ jquery.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    $('.btn1').click(function(){
+        alert("this page doesn't exist")
+    });
+});
+
+$(document).ready(function(){
+    $('.navbar_elements ul li a').click(function(){
+        $('.navbar_elements ul li a').css('text-decoration','underline');
+    });
+});
+
+$(document).ready(function(){
+    $('.card-image1').html(function(){
+        return '<video src=""https://youtu.be/209P2YndRzM?si=Pp1Z2nwD_WlMVXJh" alt="konark Temple">';
+    });
+});
+
+
+
+$(document).mouseenter(function(){
+    $('.card-image1').mouseenter(function(e){
+        clip.play();
+    })
+
+})
+$(document).mouseenter(function(){
+    $('.card-image1').mouseout(function(e){
+        clip.pause();
+    })
+
+})
+
+function play_video(card_id){
+    var card_id = $('#'+card_id);
+    var video = card_id.data('video-url')
+    // card_id.html('<iframe class="card-image1" src="'+video+'?autoplay=1" height="100%" width="100%"> </iframe>');
+    card_id.html('<iframe class="card-image1" src="'+video+'" height="100%" width="100%" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"> </iframe>');
+}
+
+function pause_video(card_id){
+    var card_id = $('#'+card_id);
+    var image = card_id.data('image-url')
+    card_id.html('<img class="card-image1" src="'+image+'">');
+}
+
